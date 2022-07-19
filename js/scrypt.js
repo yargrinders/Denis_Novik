863f31a749c359f4b55d9d946787a3e8d716ae8c
yargrinders <yargrindersp20@gmail.com>
2022-07-19 10:35:16 +0200

    Swiper

---
+++ js/scrypt.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.header__menu').click(function(){
+        $('.header__burger, .header__menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
+});
+
+const swiper = new Swiper('.swiper', {
+    
+    // Optional parameters
+    direction: 'horizontal',
+    autoplay: {
+        delay: 3500,
+    },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+});
